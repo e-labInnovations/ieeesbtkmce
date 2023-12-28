ac49ae8fb332dfe0974974100e539536f1c9afdc
e-labInnovations <mohammedashad23@gmail.com>
2023-12-28 17:17:28 +0530

    Major Events Hosted design completed

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
   - [ ] `src/blocks/about-us/save.js:9` & `src/blocks/about-us/edit.js:19` 
 - [ ] Fix `src/blocks/faq/edit.js` answer updating
 - [ ] Change DateTime picker style in `inc/events.php` `event_datetime`
+- [ ] Move all svg icons to single one and use references
+- After completion
+  - [ ] Watch `201. Templates (Part 2)` and fix the template default images
---
+++ _designs/major-events-hosted/script.js
@@ -0,0 +1,106 @@
+document.querySelectorAll('.major-events-hosted').forEach(mef => {
+    let nextBtn = mef.querySelector('#meh-next')
+    let prevBtn = mef.querySelector('#meh-prev')
+    let sliderContiner = mef.querySelector('#meh-slider-container')
+    let slider = mef.querySelector('#meh-slider')
+    let slides = slider.querySelectorAll('.slide')
+
+    let elementsToShow = 3
+    let sliderContinerWidth = sliderContiner.clientWidth
+    let slideWidth = sliderContinerWidth / elementsToShow
+
+    const mehInit = () => {
+        slider.style.marginLeft = 0
+
+        if (document.body.clientWidth > 1536) {
+            elementsToShow = 4
+        } else if (document.body.clientWidth > 1024) {
+            elementsToShow = 3
+        } else if (document.body.clientWidth > 768) {
+            elementsToShow = 2
+        } else {
+            elementsToShow = 1
+        }
+
+        sliderContinerWidth = sliderContiner.clientWidth
+        slideWidth = sliderContinerWidth / elementsToShow
+
+        slider.style.width = slides.length * slideWidth + 'px'
+        slider.style.transition = 'margin'
+        slider.style.transitionDuration = '700ms'
+        slider.style.transitionTimingFunction = 'cubic-bezier(0.42, 0, 0.58, 1.0)'
+
+        slides.forEach(silde => {
+            silde.style.width = slideWidth + 'px'
+        })
+    }
+
+    mehInit()
+
+    window.addEventListener('resize', () => {
+        console.log('window change');
+        mehInit()
+    });
+
+    const changeButtons = () => {
+        let sliderMargin = Math.round(+slider.style.marginLeft.slice(0, -2))
+
+        const enableButton = (btn, enable) => {
+            if (enable) {
+                btn.classList.remove('border-gray-400')
+                btn.classList.add('border-primary-800')
+
+                btn.querySelector('svg').classList.remove('fill-gray-400')
+                btn.querySelector('svg').classList.add('fill-primary-800')
+            } else {
+                btn.classList.remove('border-primary-800')
+                btn.classList.add('border-gray-400')
+
+                btn.querySelector('svg').classList.remove('fill-primary-800')
+                btn.querySelector('svg').classList.add('fill-gray-400')
+            }
+        }
+
+        //Enable prevBtn
+        if (sliderMargin != 0) {
+            enableButton(prevBtn, true)
+        } else {
+            enableButton(prevBtn, false)
+        }
+
+        //Enable nextBtn
+        if (sliderMargin != Math.round(-slideWidth * (slides.length - elementsToShow))) {
+            enableButton(nextBtn, true)
+        } else {
+            enableButton(nextBtn, false)
+        }
+
+
+    }
+
+    prevBtn.addEventListener('click', () => {
+        if (Math.round(+slider.style.marginLeft.slice(0, -2)) != 0) {
+            slider.style.marginLeft = (+slider.style.marginLeft.slice(0, -2)) + slideWidth + 'px'
+        }
+
+        changeButtons()
+    })
+
+    nextBtn.addEventListener('click', () => {
+        if (Math.round(+slider.style.marginLeft.slice(0, -2)) != Math.round(-slideWidth * (slides.length - elementsToShow))) {
+            slider.style.marginLeft = (+slider.style.marginLeft.slice(0, -2)) - slideWidth + 'px'
+        }
+
+        changeButtons()
+    })
+})
+
+/*
+Avtive: 
+    Button:
+        add     -> border-primary-800
+        remove  -> border-gray-400
+    SVG:
+        add     -> fill-primary-800
+        remove  -> fill-gray-400
+*/
