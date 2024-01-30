aabcb1047afea6f5003bf8e032071681c1939138
e-labInnovations <mohammedashad23@gmail.com>
2024-01-30 22:03:38 +0530

    latest news design completed

--- _designs/front-page/script.js
+++ _designs/front-page/script.js
@@ -9,15 +9,84 @@ document.querySelectorAll("nav").forEach((nav) => {
 });
 /** Navbar End **/
 
-/** Latest Events Start **/
+/** Latest News Start **/
 document.addEventListener("DOMContentLoaded", function (event) {
   document
     .querySelectorAll("#latest-news-section")
     .forEach((latestNewsSection) => {
       const nextButton = latestNewsSection.querySelector("#news-next");
       const prevButton = latestNewsSection.querySelector("#news-prev");
-      const activeImg = latestNewsSection.querySelector(".news-front-img");
-      const imagesDiv = activeImg.parentElement;
+      const imagesClasses = {
+        default: ".news-item",
+        front: "news-front-img",
+        back: "news-back-img",
+        backHidden: "news-hidden-back-img",
+        frontHidden: "news-hidden-front-img",
+      };
+      const slideDiv = latestNewsSection.querySelector(
+        imagesClasses.default,
+      ).parentElement;
+      const newsItems = latestNewsSection.querySelectorAll(
+        imagesClasses.default,
+      );
+      const disabledClasses = ["pointer-events-none", "opacity-50"];
+      const activeNewsItem = {
+        title: latestNewsSection.querySelector("#active-news-title"),
+        content: latestNewsSection.querySelector("#active-news-content"),
+        details: latestNewsSection.querySelector("#active-news-details"),
+        register: latestNewsSection.querySelector("#active-news-register"),
+      };
+      let activePos = 0;
+
+      const setContents = (newsItem) => {
+        const contentP = newsItem.querySelector("p");
+        const title = contentP.dataset.title;
+        const moreDetailsUrl = contentP.dataset.detailsUrl;
+        const registerUrl = contentP.dataset.registerUrl;
+
+        activeNewsItem.title.innerText = title;
+        activeNewsItem.content.innerText = contentP.innerText.trim();
+        activeNewsItem.details.href = moreDetailsUrl ? moreDetailsUrl : "#";
+        if (moreDetailsUrl == "#") {
+          activeNewsItem.details.classList.add(...disabledClasses);
+        } else {
+          activeNewsItem.details.classList.remove(...disabledClasses);
+        }
+        activeNewsItem.register.href = registerUrl ? registerUrl : "#";
+        if (registerUrl == "#") {
+          activeNewsItem.register.classList.add(...disabledClasses);
+        } else {
+          activeNewsItem.register.classList.remove(...disabledClasses);
+        }
+      };
+
+      const setImageClasses = () => {
+        newsItems.forEach((newsItem, i) => {
+          newsItem.classList.remove(
+            imagesClasses.frontHidden,
+            imagesClasses.front,
+            imagesClasses.back,
+            imagesClasses.backHidden,
+            "hidden",
+          );
+
+          if (i === activePos) {
+            newsItem.classList.add(imagesClasses.front);
+            setContents(newsItem);
+          } else if (i === (activePos + 1) % newsItems.length) {
+            newsItem.classList.add(imagesClasses.back);
+          } else if (
+            i === activePos - 1 ||
+            (activePos === 0 && i === newsItems.length - 1)
+          ) {
+            newsItem.classList.add(imagesClasses.frontHidden);
+          } else {
+            newsItem.classList.add(imagesClasses.backHidden);
+          }
+        });
+      };
+
+      setImageClasses();
 
       const lnInit = () => {
         const activeImg = latestNewsSection.querySelector(".news-front-img");
@@ -33,50 +102,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
       });
 
       const doNext = () => {
-        const activeImg = latestNewsSection.querySelector(".news-front-img");
-        const nextImg = latestNewsSection.querySelector(".news-back-img");
-        const hiddenBackImg = latestNewsSection.querySelector(
-          ".news-hidden-back-img",
-        );
-        const hiddenFrontImg = latestNewsSection.querySelector(
-          ".news-hidden-front-img",
-        );
-
-        activeImg.classList.replace("news-front-img", "news-hidden-front-img");
-        nextImg.classList.replace("news-back-img", "news-front-img");
-        hiddenBackImg.classList.replace(
-          "news-hidden-back-img",
-          "news-back-img",
-        );
-        hiddenFrontImg.classList.replace(
-          "news-hidden-front-img",
-          "news-hidden-back-img",
-        );
+        activePos++;
+        activePos = activePos >= newsItems.length ? 0 : activePos;
+        setImageClasses();
       };
 
       const doPrev = () => {
-        const activeImg = latestNewsSection.querySelector(".news-front-img");
-        const nextImg = latestNewsSection.querySelector(".news-back-img");
-        const hiddenBackImg = latestNewsSection.querySelector(
-          ".news-hidden-back-img",
-        );
-        const hiddenFrontImg = latestNewsSection.querySelector(
-          ".news-hidden-front-img",
-        );
-
-        activeImg.classList.replace("news-front-img", "news-back-img");
-        nextImg.classList.replace("news-back-img", "news-hidden-back-img");
-        hiddenBackImg.classList.replace(
-          "news-hidden-back-img",
-          "news-hidden-front-img",
-        );
-        hiddenFrontImg.classList.replace(
-          "news-hidden-front-img",
-          "news-front-img",
-        );
+        activePos--;
+        activePos = activePos < 0 ? newsItems.length - 1 : activePos;
+        setImageClasses();
       };
 
-      var mc = new Hammer(imagesDiv);
+      var mc = new Hammer(slideDiv);
       mc.on("swipeleft swiperight", function (ev) {
         if (ev.type == "swipeleft") {
           doNext();
@@ -89,7 +126,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
       prevButton.addEventListener("click", doPrev);
     });
 });
-/** Latest Events End **/
+/** Latest News End **/
 
 /** Front Gallery Start **/
 gsap.utils.toArray(".marquee").forEach((marquee) => {
