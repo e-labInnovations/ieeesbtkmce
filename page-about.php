7f456f4e03639368e7cf61f59dfde77cc8673c4c
e-labInnovations <mohammedashad23@gmail.com>
2024-02-19 23:16:49 +0530

    padding fix

--- page-about.php
+++ page-about.php
@@ -5,12 +5,12 @@ Template Name: About Us
 
 get_header(null, array('mode' => 'light')); ?>
 
-<main class="container mx-auto px-4 xl:px-14">
-    <h1 class="font-poppins text-5xl md:mt-24 md:mb-16 mt-12 mb-4 font-semibold text-primary-800 sm:text-7xl">
-        <?php the_title() ?>
-    </h1>
+<main class="container pt-[72px] mx-auto px-4 xl:px-14">
+  <h1 class="font-poppins text-5xl md:mt-24 md:mb-16 mt-12 mb-4 font-semibold text-primary-800 sm:text-7xl">
+    <?php the_title() ?>
+  </h1>
 
-    <?php
+  <?php
         if(have_posts()) {
             while (have_posts()) {
                 the_post();
